6c478128852f807abfad3a2d7cd4e26f8bdad88b
Manel Roca <manu2187@hotmail.com>
2015-05-26 12:21:04 +0200

    WS funciona, falta estructurar i guardar a la  BBDD

--- app/src/main/java/org/projectes/androidshopping/Constants/Constants.java
+++ app/src/main/java/org/projectes/androidshopping/Constants/Constants.java
@@ -2,7 +2,10 @@ package org.projectes.androidshopping.Constants;
 
 /**
  * Created by mrr on 12/05/15.
+ * Classe per guardar les constants de la app
  */
 public class Constants {
     public static final String URL_PRODUCTES = "http://www.v2msoft.com/curso-android/ws/lista_productos.php";
+    public static final int POSICIO_CONTEXT_WSTask = 0;
+    public static final int POSICIO_URL_WSTask = 1;
 }
--- app/src/main/java/org/projectes/androidshopping/Task/WSTask.java
+++ app/src/main/java/org/projectes/androidshopping/Task/WSTask.java
@@ -3,42 +3,53 @@ package org.projectes.androidshopping.Task;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Message;
+import android.widget.Toast;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.projectes.androidshopping.Constants.Constants;
 import org.projectes.androidshopping.DAObject.Product;
 import org.projectes.androidshopping.Listeners.IResult;
+import org.projectes.androidshopping.WS.JacksonJSONHelper;
 import org.projectes.androidshopping.WS.WSConnector;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by mrr on 29/04/15.
+ * AsyncTask encarregada de recuperar la informació del WS
  */
 public class WSTask extends AsyncTask<Object, Integer, Message> {
     private Message Mess;
     private Context context;
-    private ObjectMapper objMapper;
-    private Product result;
     private IResult<Message> listener;
+    private ObjectMapper mapper;
+    private List<Product> productes;
 
     @Override
     protected Message doInBackground(Object... params) {
         WSConnector WS = new WSConnector();
-        this.context = (Context)params[0];
+        this.context = (Context)params[Constants.POSICIO_CONTEXT_WSTask];
         this.Mess = null;
         try {
-            Mess = WS.get((String)params[1], null);
+            this.Mess = WS.get((String)params[Constants.POSICIO_URL_WSTask], null);
+            this.mapper = JacksonJSONHelper.Initialize();
+            this.productes = this.mapper.readValue(
+                    (String)this.Mess.obj,
+                    mapper.getTypeFactory().constructCollectionType(
+                            List.class, Product.class));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return Mess;
+        return this.Mess;
     }
 
     public void onPostExecute(Message message){
         if (this.listener != null){
             if (message.what == 200){
-                listener.onSuccess(message);
+                //listener.onSuccess(message);
+                Toast.makeText(this.context, productes.get(2).getNombre(), Toast.LENGTH_LONG).show();
             }else{
                 listener.onFail("Error HTTP");
             }

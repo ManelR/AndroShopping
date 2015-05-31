daa6e1bd526d8b58357afd941c215d043e737aa0
Manel Roca <manu2187@hotmail.com>
2015-05-31 23:57:04 +0200

    Fragment1 ++

--- app/src/main/java/org/projectes/androidshopping/fragments/NewProductFragment1.java
+++ app/src/main/java/org/projectes/androidshopping/fragments/NewProductFragment1.java
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import org.projectes.androidshopping.R;
 
 public class NewProductFragment1 extends Fragment {
     private View fragmentView;
+    private EditText txtName;
+    private EditText txtDesc;
+
 
     public NewProductFragment1(){}
 
@@ -17,7 +21,16 @@ public class NewProductFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         this.fragmentView = inflater.inflate(R.layout.fragment_new_product_fragment1, container, false);
+        this.txtName = (EditText)fragmentView.findViewById(R.id.fragment_newProduct1_txtName);
+        this.txtDesc = (EditText)fragmentView.findViewById(R.id.fragment_newProduct1_txtDesc);
         return this.fragmentView;
     }
 
+    public String getTxtName(){
+        return this.txtName.getText().toString();
+    }
+
+    public String getTxtDesc(){
+        return this.txtDesc.getText().toString();
+    }
 }
